f599f48e906e913a3e236f038426f7382a6ef8bf
Dima Spodenyuk <dimonsmail8@gmail.com>
2015-03-03 09:47:36 +0200

    first commit

---
+++ share.js
@@ -0,0 +1,47 @@
+if (Meteor.isClient) {
+    Template.customUpload.created = function () {
+        Uploader.init(this);
+    }
+
+    Template.customUpload.rendered = function () {
+        Uploader.render.call(this);
+    };
+
+    Template.customUpload.events({
+        'click .start': function (e) {
+            Uploader.startUpload.call(Template.instance(), e);
+        }
+    });
+
+    Template.customUpload.helpers({
+        'infoLabel': function () {
+            var instance = Template.instance();
+
+            // we may have not yet selected a file
+            var info = instance.info.get()
+            if (!info) {
+                return;
+            }
+
+            var progress = instance.globalInfo.get();
+
+            // we display different result when running or not
+            return progress.running ?
+                info.name + ' - ' + progress.progress + '% - [' + progress.bitrate + ']' :
+                info.name + ' - ' + info.size + 'B';
+        },
+        'progress': function () {
+            return Template.instance().globalInfo.get().progress + '%';
+        }
+    })
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        UploadServer.init({
+            tmpDir: process.env.PWD + '/.uploads/tmp',
+            uploadDir: process.env.PWD + '/.uploads/',
+            checkCreateDirectories: true
+        });
+    });
+}
